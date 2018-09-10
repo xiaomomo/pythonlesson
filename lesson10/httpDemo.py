93da212eda77a9ff794ffb7317beab179a67a24a
xiaomomo <woshilqj@gamil.com>
2018-09-10 11:41:32 +0800

    first commit

--- lesson10/httpDemo.py
+++ lesson10/httpDemo.py
@@ -1,1 +1,9 @@
-# 命令行新闻阅读器
+import requests
+
+headers = {'Authorization': 'APPCODE 2c571bbe36b24e9aa3cadaaee4d0adbf'}
+
+r = requests.get(
+    'https://saweather.market.alicloudapi.com/spot-to-weather?area=杭州&need3HourForcast=0&needAlarm=0&needHourData=0&needIndex=0&needMoreDay=0',
+    headers=headers)
+
+print(r.json()['showapi_res_body']['now']['weather'])
